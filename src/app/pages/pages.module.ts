293f4156ec5907c74a3189a4d6269d118bd8fa11
Victor Boveda Prado <vbpradoo@gmail.com>
2023-08-10 16:44:19 -0500

    Fin seccion 8

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -10,6 +10,8 @@ import { Grafica1Component } from '../pages/grafica1/grafica1.component';
 import { PagesComponent } from '../pages/pages.component';
 import { ComponentsModule } from '../components/components.module';
 import { AccountSettingsComponent } from './account-settings/account-settings.component';
+import { PromesasComponent } from './promesas/promesas.component';
+import { RxjsComponent } from './rxjs/rxjs.component';
 
 @NgModule({
   declarations: [
@@ -17,7 +19,9 @@ import { AccountSettingsComponent } from './account-settings/account-settings.co
     ProgressComponent,
     Grafica1Component,
     PagesComponent,
-    AccountSettingsComponent
+    AccountSettingsComponent,
+    PromesasComponent,
+    RxjsComponent
   ],
   exports: [
     DashboardComponent,
--- src/app/services/sidebar.service.ts
+++ src/app/services/sidebar.service.ts
@@ -11,8 +11,10 @@ export class SidebarService {
       icono:  'mdi mdi-gauge',
       submenu: [
         { titulo: 'Main', url: '/'},
-        { titulo: 'ProgressBar', url: 'progress'},
         { titulo: 'Graficas', url: 'grafica1'},
+        { titulo: 'Promesas', url: 'promesas'},
+        { titulo: 'ProgressBar', url: 'progress'},
+        { titulo: 'RxJs', url: 'rxjs'},
       ]
 
     }
